0769e7edfaa4928a27740486b9dd8c57480017aa
Kai Schaller <kschaller@gmail.com>
2018-06-17 01:48:38 -0700

    Create Element enum to more easily map XML

---
+++ Podcatcher/Element.swift
@@ -0,0 +1,16 @@
+//
+//  Element.swift
+//  Podcatcher
+//
+//  Created by Kai Schaller on 6/17/18.
+//  Copyright © 2018 Kai Schaller. All rights reserved.
+//
+
+import Foundation
+
+enum Element: String {
+    case item
+    case title
+    case date = "pubDate"
+    case enclosure
+}
