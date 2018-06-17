f8b1ae233d25551353f6f1535875c1a4b5ed9206
Kai Schaller <kschaller@gmail.com>
2018-06-17 16:24:14 -0700

    Create `OperationQueue` and handle saving episodes to disk

--- Podcatcher/Podcatcher.swift
+++ Podcatcher/Podcatcher.swift
@@ -10,18 +10,41 @@ import Foundation
 
 class Podcatcher {
     
-    let consoleIO = ConsoleIO()
-    var parser: Parser?
+    private let consoleIO = ConsoleIO()
+    private let downloadQueue: OperationQueue = {
+        let queue = OperationQueue()
+        queue.qualityOfService = .utility
+        queue.maxConcurrentOperationCount = 3
+        return queue
+    }()
+    private let outputDateFormatter: DateFormatter = {
+       let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        return formatter
+    }()
     
+    private var parser: Parser?
+    private var outputURL: URL?
+    private var downloadCount: (new: Int, skipped: Int) = (0, 0)
+
     func staticMode() {
 //        consoleIO.printUsage()
         guard let url = URL(string: CommandLine.arguments[1]) else {
             consoleIO.writeMessage("Unable to read URL", to: .error)
             return
         }
+        
+        outputURL = URL(fileURLWithPath: CommandLine.arguments[2]) // TODO: Add some checking so we don't get OOB error
+        
         parser = Parser(url: url)
         parser?.delegate = self
         parser?.parse()
+    }
+    
+    fileprivate func outputURL(for episode: Episode) -> URL? {
+        let dateString = outputDateFormatter.string(from: episode.date)
+        let filename = "\(dateString)_\(episode.title).\(episode.fileExtension)"
+        return outputURL?.appendingPathComponent(filename)
     }
     
 }
@@ -30,6 +53,36 @@ extension Podcatcher: ParserDelegate {
     
     func finishedParsing(episodes: [Episode]) {
         // Load the episodes into a download queue.
+        for episode in episodes {
+            guard let outputURL = outputURL(for: episode) else {
+                continue
+            }
+            
+            if FileManager.default.fileExists(atPath: outputURL.path) == false {
+                let operation = DownloadOperation(episode: episode)
+                operation.delegate = self
+                downloadQueue.addOperation(operation)
+            } else {
+                downloadCount.skipped += 1
+            }
+        }
+        downloadQueue.waitUntilAllOperationsAreFinished()
+        consoleIO.writeMessage("Done! Downloaded \(downloadCount.new) new episodes and skipped \(downloadCount.skipped) that had already been saved.")
+    }
+    
+}
+
+extension Podcatcher: DownloadOperationDelegate {
+    
+    func didFinishDownloading(episode: Episode, temporaryURL: URL) {
+        guard let outputFileURL = outputURL(for: episode) else { return }
+
+        do {
+            try FileManager.default.copyItem(at: temporaryURL, to: outputFileURL)
+            downloadCount.new += 1
+        } catch(let error) {
+            print("Error writing to \(outputFileURL.absoluteString): \(error.localizedDescription)")
+        }
     }
     
 }
