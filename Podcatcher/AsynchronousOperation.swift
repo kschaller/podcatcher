28697512474d3f7e27070c3c029b77aa40c6f9a0
Kai Schaller <kschaller@gmail.com>
2018-06-17 16:22:29 -0700

    Refactor asynchronous `Operation` subclass

--- Podcatcher/AsynchronousOperation.swift
+++ Podcatcher/AsynchronousOperation.swift
@@ -5,46 +5,55 @@
 //  Created by Kai Schaller on 5/25/18.
 //  Copyright © 2018 Kai Schaller. All rights reserved.
 //
+//  Inspired by Vasily Ulianov at https://gist.github.com/Sorix/57bc3295dc001434fe08acbb053ed2bc
+//
 
 import Foundation
 
+/// Subclass of `Operation` that adds support for asynchronicity.
+/// ## How to use:
+/// - Call `super.main()` when overriding `main` method.
+/// - Call `super.start()` when overriding `start` method.
+/// - When the operation is finished or cancelled, set `self.state = .finished`.
 class AsynchronousOperation: Operation {
     
-    private let stateQueue = DispatchQueue(label: "com.kaischaller.operation.state", attributes: .concurrent)
+    enum State: String {
+        case ready = "Ready"
+        case executing = "Executing"
+        case finished = "Finished"
+        fileprivate var keyPath: String { return "is\(self.rawValue)" }
+    }
     
-    private var rawState = OperationState.ready
+    override var isAsynchronous: Bool { return true }
+    override var isExecuting: Bool { return state == .executing }
+    override var isFinished: Bool { return state == .finished }
     
-    private var state: OperationState {
-        get {
-            return stateQueue.sync(execute: { rawState })
+    var state = State.ready {
+        willSet {
+            willChangeValue(forKey: state.keyPath)
+            willChangeValue(forKey: newValue.keyPath)
         }
-        set {
-            willChangeValue(forKey: "state")
-            stateQueue.sync(flags: .barrier, execute: { rawState = newValue })
-            didChangeValue(forKey: "state")
+        didSet {
+            didChangeValue(forKey: state.keyPath)
+            didChangeValue(forKey: oldValue.keyPath)
         }
     }
     
-    final override var isReady: Bool {
-        return state == .ready && super.isReady
+    override func start() {
+        if isCancelled {
+            state = .finished
+        } else {
+            state = .ready
+            main()
+        }
     }
     
-    final override var isExecuting: Bool {
-        return state == .executing
+    override func main() {
+        if self.isCancelled {
+            state = .finished
+        } else {
+            state = .executing
+        }
     }
-    
-    final override var isFinished: Bool {
-        return state == .finished
-    }
-    
-    final override var isAsynchronous: Bool {
-        return true
-    }
-    
-}
 
-private enum OperationState: Int {
-    case ready
-    case executing
-    case finished
 }
