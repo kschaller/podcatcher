fcc9609ddfc9bed595bc307fc556da2f12669622
Kai Schaller <kschaller@gmail.com>
2018-06-17 16:23:47 -0700

    Implement async download operation for episodes

--- Podcatcher/DownloadOperation.swift
+++ Podcatcher/DownloadOperation.swift
@@ -8,8 +8,33 @@
 
 import Foundation
 
-class DownloadOperation: Operation {
+protocol DownloadOperationDelegate: class {
+    func didFinishDownloading(episode: Episode, temporaryURL: URL)
+}
 
+class DownloadOperation: AsynchronousOperation {
+
+    weak var delegate: DownloadOperationDelegate?
     
+    private let episode: Episode
+    private let semaphore = DispatchSemaphore(value: 0)
+    
+    init(episode: Episode) {
+        self.episode = episode
+    }
+    
+    override func main() {
+        super.main()
+        
+        print(episode.url)
+        let task = URLSession.shared.downloadTask(with: episode.url) { (fileURL, response, error) in
+            guard let fileURL = fileURL else { return }
+            self.delegate?.didFinishDownloading(episode: self.episode, temporaryURL: fileURL)
+            self.semaphore.signal()
+            self.state = .finished
+        }
+        task.resume()
+        semaphore.wait()
+    }
     
 }
